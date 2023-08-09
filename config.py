168f6c5f25cd8b9dbe17a0cc7a676f15f7ec1f7e
Danishbir Singh <100703824+danishbirsingh@users.noreply.github.com>
2023-08-09 13:16:52 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+# Twilio API Credentials
+TWILIO_ACCOUNT_SID = 'your_account_sid'
+TWILIO_AUTH_TOKEN = 'your_auth_token'
+
+# Twilio Messaging Settings
+NOTIFICATION_PHONE_NUMBER = '+1234567890'  # Your phone number in E.164 format
+TWILIO_PHONE_NUMBER = '+0987654321'  # Twilio phone number in E.164 format
+
+# Detection Settings
+DETECTION_SENSITIVITY = 0.5  # Adjust sensitivity level for object detection
+
+# Repellent Settings
+USE_ULTRASONIC_REPELLENT = True  # Set to True if using ultrasonic repellent
+USE_LED_LIGHTS = False  # Set to True if using LED lights as repellent
+# Add more settings here for different repellent mechanisms
+
+# Email Settings
+SEND_EMAIL_NOTIFICATIONS = False  # Set to True if you want to send email notifications
+EMAIL_SENDER = 'your_email@example.com'
+EMAIL_RECEIVER = 'receiver@example.com'
+SMTP_SERVER = 'smtp.example.com'
+SMTP_PORT = 587
+SMTP_USERNAME = 'your_username'
+SMTP_PASSWORD = 'your_password'
